10a4e2e3a8c61ad9ff64fbcd82a84ed928462976
WEI-JUN TANG <after062111@gmail.com>
2020-09-18 18:50:02 -0600

    first push

---
+++ cse341-project-master/routes/pr01.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.render('pages/pr01/form', {
+        title: 'Form page',
+        path: '/pr01', // For pug, EJS 
+        activeTA03: true, // For HBS
+        contentCSS: true, // For HBS
+    });
+});
+
+router.post('/submit', (req, res, next) => {
+    //console.log(req.body.i1)
+    //console.log(req.body.i1)
+    res.render('pages/pr01/display', {
+        title: 'display page',
+        path: '/pr01', // For pug, EJS 
+        activeTA03: true, // For HBS
+        contentCSS: true, // For HBS
+        newText1: req.body.newText1,
+        newText2: req.body.newText2,
+    });
+});
+.
+module.exports = router;
